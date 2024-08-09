9cdac738107eea9533dc9eabcee45655fc11c6f4
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-09 19:21:45 +0700

    Edit reverseString function to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const charList = string.split('');
+    for (let i = 0, n = charList.length; i < n / 2; i++) {
+        const tmp = charList[i];
+        charList[i] = charList[n - i - 1];
+        charList[n - i - 1] = tmp;
+    }
+    return string = charList.join('');
 };
 
 // Do not edit below this line

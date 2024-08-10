ec4dcf01389b082c095195ea74847ef73a45aed6
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-10 17:10:03 +0700

    Edit leap year calculation function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,8 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 !== 0) return false;
+    
+    return year % 100 !== 0 ? true :
+    year % 400 === 0 ? true : false;
 
 };
 

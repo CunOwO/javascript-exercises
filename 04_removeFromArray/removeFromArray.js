d7e79721278a8782d678bafe008684d7b782b24c
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-10 14:34:11 +0700

    Edit removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToBeRemoved) {
+    for (let i = 0, length = valuesToBeRemoved.length; i < length; i++) {
+        let removalCount = 0;
+        let removalIndex;
+        for (let j = 0, length = array.length; j < length; j++) {
+            if (array[j] === valuesToBeRemoved[i]) {
+                removalIndex = j - removalCount;
+                removalCount += 1;
+            }
+        }
+        array.splice(removalIndex, removalCount);
+    }
+    return array;
 };
 
 // Do not edit below this line

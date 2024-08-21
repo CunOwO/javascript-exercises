414298f8f9eb41bf34413f10958588357194cde3
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-21 20:59:53 +0700

    Edit findTheOldest function using map() and sort()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.map((person) => {
+        if (!("yearOfDeath" in person)) {
+            person["yearOfDeath"] = new Date().getFullYear();
+        }
+        return person;
+    })
+    .sort((a, b) => {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return nextGuy - lastGuy;
+      });
+    return oldest[0];
 };
 
 // Do not edit below this line

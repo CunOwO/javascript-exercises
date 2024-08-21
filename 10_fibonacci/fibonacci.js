bdb58999422a8d87056990a53239f578dc147d3c
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-21 17:34:09 +0700

    Edit Fibonacci function to return specific sequence member

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if (index === 0) {
+        return 0;
+    }
+    if (index < 0) {
+        return "OOPS";
+    }
+    const array = [1, 1];
+    for (let i = 0; i < index - 2; i++) {
+        const item = array[i] + array[i + 1];
+        array.push(item);
+    }
+    return array[index - 1];
 };
 
 // Do not edit below this line

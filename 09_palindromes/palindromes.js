beb451602442d9ebca2ee748dceea6c080dbb1c1
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-21 16:50:16 +0700

    Edit the function that determines a palindrome starting
    - Focus on using array methods.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+const originalArray = string.toLowerCase().split("");
+const plainArray = originalArray.filter((char) => char >= "A" && char <= "Z" || char >= "a" && char <= "z" || char >= "0" && char <= "9" );
+const cloneArray = plainArray.slice();
+cloneArray.reverse();
 
+return cloneArray.length === plainArray.length && cloneArray.every((char, index) => char === plainArray[index]);
 };
 
 // Do not edit below this line
